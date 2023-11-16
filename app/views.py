6ca345c7b69c3f9bedafa45a4bf4262babc339b8
DasariRaju7 <dasariraju713@gmail.com>
2023-11-16 15:26:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def macha(request):
+    return render(request,'one.html')
+
+def macha1(request):
+    return render(request,'one.html')
+
+def macha2(request):
+    return render(request,'one.html')
+
+def macha3(request):
+    return render(request,'one.html')
+ 
